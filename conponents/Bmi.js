55b93901c36fa93dec3abc9fec749df8a6b343da
sol2kwon <amorfatisole@gmail.com>
2022-02-24 20:34:12 +0900

    2022-02-21 20:34

--- conponents/Bmi.js
+++ conponents/Bmi.js
@@ -3,39 +3,44 @@ import Layout from '../containers/Layout';
 import axios from 'axios';
 export default function Bmi(){
     
-    const [username, setUsername] = useState("")
-    const [height, setHeight] = useState(0.0)
-    const [weight, setWeight] = useState(0.0)
-    
-    const getBmi = async (e) => {
-      e.preventDefault()    
-      alert('버튼 클릭')
-        try{
-            await axios.get(`http://localhost:8080/member/bmi/홍길동/180.5/80.5`)
+    const [inputs, setInputs] = useState({})
+     const {username,weight,height} = inputs; // Objext Destructuring
+
+    const handleChange =(e)=>{
+        e.preventDefault()
+        const {value,name} = e.target;
+        setInputs({
+            ...inputs,[name]:value})
+        }
+
+    const handleClick =(e)=>{
+        e.preventDefault()
+        const bmiRequest = {username, weight, height}
+        alert(`사용자이름: ${JSON.stringify(bmiRequest)}`)
+        /*alert(`사용자이름: ${JSON.stringify(bmiRequest)}`)
+            axios.get(`http://localhost:8080/member/bmi/김길동/180.5/80.5`)
             .then((res)=>{
               alert(`답장이 도착했습니다 [내용] ${JSON.stringify(res.data)}`)
-            })
-
-        }catch(error){
-          alert(`에러가 발생했습니다. [내용] ${error}`)
-
+            })*/
+            
         }
-    }
   
-    return <Layout><h1>Bmi폼</h1>
+    return (<Layout>
+        <form><h1>Bmi폼</h1>
     
     
     <div>
     <label><b>Username</b></label>
-    <input id="" type="" /><br />
+    <input type="text"  onChange ={handleChange} name="username"/><br />
 
     <label htmlFor=""><b>height</b></label>
-    <input id= "" type="" /><br />
+    <input type= "text" onChange ={handleChange} name="height"/><br/>
 
     <label htmlFor=""><b>weight</b></label>
-    <input type= "button"  onClick={getBmi} value="BMI 체크" /><br />
+    <input type= "text"  onChange ={handleChange} name="weight"/><br />
+    <button onClick={handleClick}>BMI 체크</button>
     </div>
+    </form>
    
-    </Layout>
-     
-}
+    </Layout>)
+    }
--- conponents/Calc.js
+++ conponents/Calc.js
@@ -1,39 +1,33 @@
 import React, { useState } from "react";
 import Layout from "../containers/Layout";
 export default function Calc(){
-    const[num1,setNum1]=useState(0);
-    const[num2,setNum2]=useState(0);
-    const[opcode,setOpcode]=useState("")
-    const[result,setResult]=useState(0)
-    const sum =()=>{
-        let num1 = document.getElementById('num1').value
-        console.log('숫자1:'+num1)
-        let num2 = document.getElementById('num2').value
-        console.log('숫자2:'+num2)
-        setNum1(num1)
-        setNum2(num2)
-       
-        setResult(Number(num1)+Number(num2))
-        console.log('결과:'+result)
+const [inputs,setInputs]=useState({})
+const {num1,opcode,num2}=inputs;
+
+const handleChange =(e)=>{
+    e.preventDefault()
+    const {value,name}=e.target;
+    setInputs({
+        ...inputs,[name]:value})
     }
 
-return <Layout><h1>계산기 어플</h1>
-<form action="">
-<label><b>숫자1</b></label>
-<input id="num1"type=""/><br />
-<label><b>  연산자</b></label>
-<select name="" id="">
-    <option value="">+</option>
-    <option value="">-</option>
-    <option value="">*</option>
-    <option value="">/</option>
-    <option value="">%</option>
-</select>
-<br />
-<label><b> 숫자2 </b></label>
-<input id="num2"type=""/><br />
-<button onClick={()=>{sum()}}>더하기 실행</button>
+const handleClick=(e)=>{
+    e.preventDefault()
+    const calcRequest = {num1,opcode,num2}
+    alert(`사용자이름: ${JSON.stringify(calcRequest)}`)
+    }
+    return (<Layout><form><h1>계산기 폼</h1>
+<div>
+<label><b>num1</b></label>
+<input type="text" onChange = {handleChange} name="num1"/><br />
+
+<label><b>opcode</b></label>
+<input type="text" onChange = {handleChange} name="opcode"/><br />
+
+<label><b>num2</b></label>
+<input type="text" onChange = {handleChange} name="num2"/><br />
+<button onClick={handleClick}>Calc 체크</button>
+</div>
 </form>
-<div>결과: {num1}+{num2}={result}</div>
-</Layout>
+</Layout>)
 }
